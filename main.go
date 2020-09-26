8c53684f3d7fb0ec689d8617a659335b85362c73
Quentin RIBAC <ribac.quentin@gmail.com>
2020-09-26 21:15:45 +0200

    hmm

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	r.GET("/:pslug", ProjectGET)
 	r.POST("/:pslug", ProjectPOST)
 
-	r.Run(":443")
+	r.Run("autochrone.herokuapp.com:443")
 }
 
 // RootGET project creation form + about
